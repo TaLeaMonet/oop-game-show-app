afdd294128e38c857ab9229e122b04089654e44e
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-06-25 13:50:14 -0700

    create conditional for space and letter placeholders.

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -6,6 +6,20 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
     }
     addPhraseToDisplay() {
-        
+        const phraseArr = this.phrase.split("");
+        const phrase_ul = document.querySelector("ul");
+        for(let i = 0; i < phraseArr.length; i++) {
+            if(phraseArr[i] === " "){
+                const space_li = document.createElement("li");
+                space_li.className = "space";
+                phrase_ul.append(space_li);
+            } else {   
+                const letter_li = document.createElement("li");
+                letter_li.className = "hide letter";
+                letter_li.textContent = `${phraseArr[i]}`;
+                phrase_ul.append(letter_li);
+            }
+        }
+        return phrase_ul; 
     }
 }
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,12 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-  };
-  const game = new Game();
-  logPhrase(game.getRandomPhrase());
-  logPhrase(game.getRandomPhrase());
-  logPhrase(game.getRandomPhrase());
-  logPhrase(game.getRandomPhrase());
-  logPhrase(game.getRandomPhrase());
+const game = new Game();
+game.getRandomPhrase().addPhraseToDisplay();
+console.log(phrase);
