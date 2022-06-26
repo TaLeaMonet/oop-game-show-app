091b89f0025c92ae24f097ca04f7e5609886f86b
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-06-26 13:40:50 -0700

    create event listenter for start button.

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,6 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game = null;  
+const startButton = document.getElementById('btn__reset');
+
+startButton.addEventListener("click", () => {
+    game = new Game().startGame();
+});
